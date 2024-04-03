d8b99c2890149a72f58372830ebf517cc85d21f0
dhnascimento <danielhn@gmail.com>
2024-04-03 17:24:40 -0400

    add proper tsx extension to componets

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
-import { Container, Flex, VStack } from "@chakra-ui/react";
-import Details from "../src/sections/details.jsx";
-import Cart from "../src/sections/cart.jsx";
+import { Container, Flex } from "@chakra-ui/react";
+import Details from "../src/sections/details";
+import Cart from "../src/sections/cart";
 
 const IndexPage = () => (
     <Container maxW="container.xl" p={0}>
--- src/sections/cart.tsx
+++ src/sections/cart.tsx
@@ -1,80 +1,74 @@
 import {
-  Heading,
-  HStack,
-  VStack,
-  Image,
-  AspectRatio,
-  Text,
-  Divider,
-  Stack,
-  Button,
-  useColorMode,
-  useColorModeValue,
-} from '@chakra-ui/react';
+    VStack,
+    Heading,
+    Divider,
+    Text,
+    Image,
+    HStack,
+    Stack,
+    AspectRatio,
+    Button,
+    useColorMode,
+    useColorModeValue
+} from "@chakra-ui/react";
 
 const Cart = () => {
-  const { toggleColorMode } = useColorMode();
-  const bgColor = useColorModeValue('gray.50', 'whiteAlpha.50');
-  const secondaryTextColor = useColorModeValue('gray.600', 'gray.400');
-  return (
-    <VStack
-      w="full"
-      h="full"
-      p={10}
-      spacing={6}
-      align="flex-start"
-      bg={bgColor}
-    >
-      <VStack alignItems="flex-start" spacing={3}>
-        <Heading size="2xl">Your cart</Heading>
-        <Text>
-          If the price is too hard on your eyes,{' '}
-          <Button onClick={toggleColorMode} variant="link" colorScheme="black">
-            try changing the theme.
-          </Button>
-        </Text>
-      </VStack>
-      <HStack spacing={6} alignItems="center" w="full">
-        <AspectRatio ratio={1} w={24}>
-          <Image src="/images/skateboard.jpg" alt="Skateboard" />
-        </AspectRatio>
-        <Stack
-          spacing={0}
-          w="full"
-          direction="row"
-          justifyContent="space-between"
-          alignItems="center"
+    const { toggleColorMode } = useColorMode();
+    const bgColor = useColorModeValue("gray.50", "whiteAlpha.50");
+    const secondaryTextColor = useColorModeValue("gray.600", "grey.400");
+    return (
+        <VStack
+            w="full"
+            h="full"
+            p={10}
+            spacing={10}
+            alignItems="flex-start"
+            bg={bgColor}
         >
-          <VStack w="full" spacing={0} alignItems="flex-start">
-            <Heading size="md">Penny board</Heading>
-            <Text color={secondaryTextColor}>PNYCOMP27541</Text>
-          </VStack>
-          <Heading size="sm" textAlign="end">
-            $119.00
-          </Heading>
-        </Stack>
-      </HStack>
-      <VStack spacing={4} alignItems="stretch" w="full">
-        <HStack justifyContent="space-between">
-          <Text color={secondaryTextColor}>Subtotal</Text>
-          <Heading size="sm">$119.00</Heading>
-        </HStack>
-        <HStack justifyContent="space-between">
-          <Text color={secondaryTextColor}>Shipping</Text>
-          <Heading size="sm">$19.99</Heading>
-        </HStack>
-        <HStack justifyContent="space-between">
-          <Text color={secondaryTextColor}>Taxes (estimated)</Text>
-          <Heading size="sm">$23.80</Heading>
-        </HStack>
-      </VStack>
-      <Divider />
-      <HStack justifyContent="space-between" w="full">
-        <Text color={secondaryTextColor}>Total</Text>
-        <Heading size="lg">$162.79</Heading>
-      </HStack>
-    </VStack>
-  );
-};
+            <VStack
+                spacing={3}
+                alignItems="flex-start"
+            >
+                <Heading size="2xl">Your Cart</Heading>
+                <Text>If price is too hard on your eyes,
+                    <Button variant="link" colorScheme="black" pl={1} onClick={toggleColorMode}>
+                        try changing the theme
+                    </Button>
+                </Text>
+            </VStack>
+            <HStack spacing={6} w="full" alignItems="center">
+                <AspectRatio ratio={1} w={24}>
+                    <Image boxSize="100px" src="/images/skate.jpg" alt="skateboard" />
+                </AspectRatio>
+                <Stack w="full" direction="row" justifyContent="space-between" alignItems="center">
+                    <VStack w="full" alignItems="flex-start" spacing={0}>
+                        <Heading size="sm">Penny Board</Heading>
+                        <Text>PNYCOMP27541</Text>
+                    </VStack>
+                    <Heading size="sm" textAlign="end">$119.00</Heading>
+                </Stack>
+            </HStack>
+            <VStack w="full" spacing={4} alignItems="stretch">
+                <HStack justifyContent="space-between">
+                    <Text color={secondaryTextColor}>Subtotal</Text>
+                    <Heading size="sm">$119.00</Heading>
+                </HStack>
+                <HStack justifyContent="space-between">
+                    <Text color={secondaryTextColor}>Shipping</Text>
+                    <Heading size="sm">$19.99</Heading>
+                </HStack>
+                <HStack justifyContent="space-between">
+                    <Text color={secondaryTextColor}>Taxes (estimated)</Text>
+                    <Heading size="sm">$23.80</Heading>
+                </HStack>
+                <Divider />
+                <HStack justifyContent="space-between">
+                    <Text color={secondaryTextColor}>Total</Text>
+                    <Heading size="lg">$162.79</Heading>
+                </HStack>
+            </VStack>
+        </VStack>
+    );
+}
 
 export default Cart;
